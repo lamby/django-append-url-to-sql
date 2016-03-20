7dc4ba5336fbecb385637faf6052ff235f8f988e
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-20 20:09:14 +0800

    Releasing version 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-append-url-to-sql',
     description="Appends the request URL to SQL statements in Django.",
-    version='1.0.1',
+    version='1.0.2',
     url='https://chris-lamb.co.uk/projects/django-append-url-to-sql',
 
     author='Chris Lamb',
